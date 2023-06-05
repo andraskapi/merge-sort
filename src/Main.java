7d80b044f1e207ff208de97eb1535ee5080cf2a5
andra <andras_kapitany@hotmail.hu>
2023-06-05 19:37:46 +0200

    add isSorted

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+
+        int[] testTombA = {1, 2, 3, 4, 5, 6, 7};
+        int[] testTombB = {12, 6, 21, 0, -3};
         System.out.println("Merge sort");
+        System.out.println(isSorted(testTombA));
     }
+
+
+    public static boolean isSorted(int[] tomb) {
+        for (int i = 0; i < tomb.length - 1; i++) {
+            if (tomb[i] > tomb[i + 1]) {
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 }
