82e43bab7e85597c573026a878d59e9ff537b0d4
andra <andras_kapitany@hotmail.hu>
2023-06-05 20:10:19 +0200

    ready for test

--- src/Main.java
+++ src/Main.java
@@ -20,22 +20,50 @@ public class Main {
         return true;
     }
 
-    public static int[] merge(int[] left, int[] right){
-        return null;
+    public static int[] merge(int[] left, int[] right) {
+
+        int[] array = new int[left.length + right.length];
+        int arrayIndex = 0;
+        int leftIndex = 0;
+        int rightIndex = 0;
+        while ( arrayIndex < array.length && rightIndex < right.length && leftIndex < left.length) {
+            if(left[leftIndex] < right[rightIndex]){
+                array[arrayIndex] = left[leftIndex];
+                leftIndex++;
+            }else {
+                array[arrayIndex] = right[rightIndex];
+                rightIndex++;
+            }
+            arrayIndex++;
+
+
+        }
+        while ( rightIndex < right.length){
+            array[arrayIndex] = left[leftIndex];
+            leftIndex++;
+            arrayIndex++;
+        }
+
+        while (leftIndex < left.length){
+            array[arrayIndex] = left[leftIndex];
+            leftIndex++;
+            arrayIndex++;
+        }
+
+        return array;
     }
 
-    public static int[] mergeSort(int[] array){
-        if (isSorted(array)){
+    public static int[] mergeSort(int[] array) {
+        if (isSorted(array)) {
             return array;
         }
 
-        int[] left = Arrays.copyOfRange(array,0,array.length/2);
-        int[] right = Arrays.copyOfRange(array,array.length / 2, array.length);
+        int[] left = Arrays.copyOfRange(array, 0, array.length / 2);
+        int[] right = Arrays.copyOfRange(array, array.length / 2, array.length);
 
         left = mergeSort(left);
         right = mergeSort(right);
-       int[] merged = merge(left,right);
-
+        int[] merged = merge(left, right);
 
 
         return merged;
